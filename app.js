0d59b305920dd67447607c10854a23969fde699c
phonthakorn <talomteam@gmail.com>
2018-04-03 23:34:04 +0700

    insert mod line-messaging x1

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var io = require('socket.io')(server);
 var LINEBot = require('line-messaging');
 
 var mongo = require('mongodb').MongoClient;
-
-app.use(express.static(__dirname + '/public')); 
-app.use(bot.webhook('/webhook')); 
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/index.html');
-});
 var bot = LINEBot.Client({
     channelID: '1567873891',
     channelSecret: '38bff89c9d11e6b862a5cf8ba8ca6617',
     channelAccessToken: 'Q2mqJWInhj4b9k8uqZW7PkzRzZrcxGzdqRPd15+51V7/gBrycuwUr01H/jY9Zmh+pZkbHhqcUCe+AmytovUmEN7JZfaMnyszNvb8nQ18VVrZRpWAkts3jYdQqTLJ5cjqvXhOaKssqAe786B5bkwjcgdB04t89/1O/w1cDnyilFU='
   });
+  
+app.use(express.static(__dirname + '/public')); 
+app.use(bot.webhook('/webhook'));
+
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
    if(err){
