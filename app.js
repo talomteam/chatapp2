7f8463df44a216b0c3bb61d27ecff80f134dd2c2
phonthakorn <talomteam@gmail.com>
2018-04-03 23:21:26 +0700

    insert line@

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var LINEBot = require('line-messaging');
 
 var mongo = require('mongodb').MongoClient;
 
@@ -9,6 +10,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
+var bot = LINEBot.Client({
+    channelID: '1567873891',
+    channelSecret: '38bff89c9d11e6b862a5cf8ba8ca6617',
+    channelAccessToken: 'Q2mqJWInhj4b9k8uqZW7PkzRzZrcxGzdqRPd15+51V7/gBrycuwUr01H/jY9Zmh+pZkbHhqcUCe+AmytovUmEN7JZfaMnyszNvb8nQ18VVrZRpWAkts3jYdQqTLJ5cjqvXhOaKssqAe786B5bkwjcgdB04t89/1O/w1cDnyilFU='
+  });
 
 mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
    if(err){
@@ -35,7 +41,10 @@ mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
             console.log(data);
         });
 
-
+        bot.on(LINEBot.Events.MESSAGE, function(replyToken, message) {
+            console.log(replyToken)
+            console.log(message)
+          });
 
 
    });
