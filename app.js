abb1183a1a24513e4fb854e832466aececbb51be
phonthakorn <talomteam@gmail.com>
2018-04-09 20:17:34 +0700

    push room ui x23

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
                     if (member_count === 0)
                     {
                         bot.getProfile(userId).then(function(data) {
-                            console.log(data)
+                           // console.log(data)
                             //dbrooms.update({"groupId":groupId},{$push:{"channel.members":data}});
                             //io.sockets.emit('messageinroom',messageEvent)
                         });
