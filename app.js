814b1447115956f231900e9ee5882bdc21f52cfe
phonthakorn <talomteam@gmail.com>
2018-04-09 19:28:25 +0700

    push room ui x11

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
                 bot.getProfile(messageEvent.source.userId).then(function(data) {
                     console.log(data)
                     dbrooms.update({"groupId":groupId},{$push:{"members":data}});
-                    roomDetail["members"] = data 
+                    roomDetail["channel"]["members"].push(data) 
                     io.sockets.emit('messageinroom',messageEvent)
                     io.sockets.emit('rooms',roomDetail)
                     
