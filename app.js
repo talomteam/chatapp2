d0591a3439664cc50b1e1d976c4a7a9cd1fbcea9
phonthakorn <talomteam@gmail.com>
2018-04-17 09:36:02 +0700

    refac code x4

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
            {
                 bot.getProfile(document.source.userId).then(function(data) {
                     document["source"]["detail"] = data;
-                    //storeMessage(messageEvent);
+                    console.log(data)
                     dbrooms.update({"groupId":document.groupId,"channel.members.userId":document.detail.userId},{$push:{"channel.members":document.detail}});
                 }).catch(function(error) {
                     console.log("getProile Error")
