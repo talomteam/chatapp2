b933dbeaab05503bed7d5b42979f44ba7341fc29
phonthakorn <talomteam@gmail.com>
2018-04-18 10:04:54 +0700

    refac code fn x21

--- app.js
+++ app.js
@@ -191,6 +191,7 @@ mongo.connect('mongodb://127.0.0.1/messaging',function(err,db){
                     document["source"]["detail"] = data.body;
                     //console.log(document)
                     dbrooms.update({"groupId":document.groupId,"channel.members.userId":document.source.userId},{$set:{"channel.members.$":document.source.detail}});
+                    storeMessage(document);
                 }).catch(function(error) {
                     console.log("getProile Error")
                     console.log(error)
